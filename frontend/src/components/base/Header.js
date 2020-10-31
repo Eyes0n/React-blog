1950562d9024215f8039182b3861f076ba91b32e
Eyes0n <eye3570@naver.com>
2020-10-31 23:09:51 +0900

    add Header component

---
+++ frontend/src/components/base/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styed from 'styled-components';
+
+const HeaderBlock = styed.div``;
+
+const Header = () => {
+  return (
+    <HeaderBlock>
+
+    </HeaderBlock>
+  );
+};
+
+export default Header;
