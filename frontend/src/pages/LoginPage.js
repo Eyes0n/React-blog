9aa7764fedc6b5e6526f417cdcae04489e45121d
Eyes0n <eye3570@naver.com>
2020-11-05 00:44:54 +0900

    apply AuthTemplate.js renamed

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import AuthTamplate from '../components/auth/AuthTamplate';
+import AuthTemplate from '../components/auth/AuthTemplate';
 import LoginForm from '../containers/auth/LoginForm';
 
 const LoginPage = () => {
   return (
-    <AuthTamplate>
+    <AuthTemplate>
       <LoginForm />
-    </AuthTamplate>
+    </AuthTemplate>
   );
 };
 
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import AuthTamplate from '../components/auth/AuthTamplate';
+import AuthTemplate from '../components/auth/AuthTemplate';
 import RegisterForm from '../containers/auth/RegisterForm';
 
 const RegisterPage = () => {
   return (
-    <AuthTamplate>
+    <AuthTemplate>
       <RegisterForm />
-    </AuthTamplate>
+    </AuthTemplate>
   );
 };
 
