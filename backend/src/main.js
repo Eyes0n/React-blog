4576f23801914757cdfa5fc3c592aa6271aae8e4
Eyes0n <eye3570@naver.com>
2020-11-15 14:36:55 +0900

    use koa-static to get files in frontend/build directory through the server

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -32,6 +35,17 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  // Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html 내용을 반환
+    // send함수를 사용하는 미들웨어 작성 -> 클라이언트 기반 라우팅이 제대로 작동하도록 함
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 
